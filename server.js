2db65dd3f876c1cc8d737c52ea00e48457128560
Alexis Newby <alexis.newby@gmail.com>
2022-03-03 06:01:19 -0700

    Another data alter for fetching array

--- server.js
+++ server.js
@@ -135,8 +135,8 @@ app.get('/api/v1/cany', (request, response) => {
 });
 
 app.get('/api/v1/care', (request, response) => {
-    // const capreef = app.locals.capreefToilets;
-    response.json(app.locals.capreefToilets)
+    const capreef = app.locals.capreefToilets;
+    response.json(capreef)
 });
 
 app.get('/api/v1/zion', (request, response) => {
