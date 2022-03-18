20b6c5ed9149e9fb8c81acae6da6d4eff412fc57
Alexis Newby <alexis.newby@gmail.com>
2022-03-18 13:35:59 -0600

    Minor fix

--- server.js
+++ server.js
@@ -146,3 +146,4 @@ app.delete('/api/v1/reviews/:id', (request, response) => {
 app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
+
